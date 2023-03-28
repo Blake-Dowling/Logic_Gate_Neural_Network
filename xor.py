8dd1801abb50f6f91de896cc6fc93c1c78badf04
_ <bdowling@ucsc.edu>
2023-03-28 13:46:42 -0700

    Implementing neural network class.

--- xor.py
+++ xor.py
@@ -41,143 +41,149 @@ def relu2DVector(vv):
 ##########################################################################################
 ##############################Initialize Input and Expected Output Data##############################
 ##########################################################################################
-inputData = xor_neural.getInputVector() #Retrieve binary combination inputs generated in xor_neural.py
-#draw_network.displayData(canvas, 2, inputData, "Input Data") #Draw actual neural network for xor
-#outputVector = xor_neural.getOutputVector() #Expected result: Retrieve xor distribution, given actual weights
-outputVector = np.array([0, 1, 1, 1]) #Expected result: Retrieve xor distribution, given actual weights
-#inputLayer = draw_network.Layer(canvas, 2, inputData, "Input") #Draw actual neural network for xor
-inputLayer.dataVector = inputData
-inputLayer.displayData() #Expected result: Retrieve xor distribution, given actual weights
+#inputData = xor_neural.getInputVector() #Retrieve binary combination inputs generated in xor_neural.py
+
+#outputVector = np.array([0, 1, 1, 1]) #Expected result: Retrieve xor distribution, given actual weights
+
+#inputLayer.dataVector = inputData
+#inputLayer.displayData() #Expected result: Retrieve xor distribution, given actual weights
+
+
 ##########################################################################################
 ##############################Neural Deep Learning Algorithm##############################
 ##########################################################################################
-bestGuess = np.full((4,), 0) #Vector to store the current best guess for display
-def learn():
-    global bestGuess
-    ##############################Display Input Data##############################
-    #canvas.create_text(10*CELL_SIZE, 1*CELL_SIZE, text = "Initial Distribution")
-    #embed_plot.embedPlot(window, 8*CELL_SIZE, 2*CELL_SIZE, 2, inputData[:, 0], inputData[:, 1])
-    #canvas.create_text(3*CELL_SIZE, 7*CELL_SIZE, text = "Hidden Layer 1 (Dot Product to Linearize)")
-    ##############################2x2 Identity Function for Linearizing Input##############################
-    id2 = np.full((2, 2), 1)
-    ##############################Linearized 'Sum' Vector##############################
-    ##############################[[0,0][0,1][1,0][1,1]].[[1,1][1,1]]->[[0,0][1,1][1,1][2,2]]##############################
-    sumVector = np.dot(inputData, id2)
-    ##############################Bias Guess Vector Initialized##############################
-    biasGuess = np.array([0.0, 0.0])
-    ##############################Weight Guess Vector Initialized##############################
-    weightGuess = np.array([0.0, 0.0])
-    #canvas.create_text(3*CELL_SIZE, 7*CELL_SIZE, text = "Hidden Layer 2 (Bias)")
-    #currentPlot, plotCanvas = embed_plot.embedPlot(window, 8*CELL_SIZE, 2*CELL_SIZE, 2, inputData[:, 0], inputData[:, 1])
-    ##############################Amount by Which Weights and Biases are Altered##############################
-    INC_AMOUNT = 0.05
+
+
+    
+
+
+   
+        ##########################################################################################
+        ##############################Apply Loss Function (Least Mean Squared)##############################
+        ##########################################################################################
+
+
+
+
+##########################################################################################
+##############################Calculate Phi (MSE) Using One Altered Weight or Bias##############################
+##########################################################################################
+def lossFunction(output, expected):
+    MSE = 0
     ##########################################################################################
-    ##############################Calculate Phi (MSE) Using One Altered Weight or Bias##############################
+    ##############################Calculate Phi (Mean Squared Error)##############################
     ##########################################################################################
-    def testParams(sumVector, weightGuess, biasGuess):
-        global bestGuess, bestText
-        MSE = 0
+    ##############################Calculate Error##############################
+    errorVector = np.subtract(output, expected)
+    ##############################Calculate Squared Error##############################
+    errorVector = np.square(errorVector)
+    ##############################Calculate Mean Squared Error##############################
+    MSE = np.sum(errorVector) / 4
+    return MSE
+
+def displayState(neural, output):
+    ##############################Draw Current Neural Network##############################
+    hiddenLayer.displayData()
+    outputLayer.dataVector = np.around(output, 2)
+    outputLayer.displayData()
+    guessPlot.addLine(neural.sumVector, output)
+    bestText = "Relu(" + str(neural.sumVector) + " + " + \
+        str(neural.params.biases) + ") * " + str(neural.params.weights) + " = " + str(output)
+    canvas.itemconfig(bestGuessLabel, text = bestText)
+    window.update()
+
+
+
+##########################################################################################
+##############################Object Representing a (4,1)->(4, ) Neural Network##############################
+##########################################################################################
+class Params:
+    def __init__(self):
+        self.weights = np.array([0.0, 0.0])
+        self.biases = np.array([0.0, 0.0])
+    def paramVector(self):
+        return np.concatenate([self.weights, self.biases])
+    def separateVector(self, paramVector):
+        weights = np.copy(paramVector[0:len(self.weights)])
+        biases = np.copy(paramVector[len(self.weights):len(paramVector)])
+        return (weights, biases)
+    def updateParams(self, paramVector):
+        self.weights, self.biases = self.separateVector(paramVector)
+    def printParams(self):
+        print("weights: " + str(self.weights))
+        print("biases: " + str(self.biases))
+class Neural:
+    def __init__(self, input):
+        self.input = input
+        self.expected = expected
+        self.bestGuess = np.full((4, ), 0)
+        ##############################2x2 Identity Function for Linearizing Input##############################
+        id2 = np.full((2, 2), 1)
+        ##############################Linearized 'Sum' Vector##############################
+        ##############################[[0,0][0,1][1,0][1,1]].[[1,1][1,1]]->[[0,0][1,1][1,1][2,2]]##############################
+        self.params = Params()
+        self.sumVector = np.dot(input, id2)
+        ##############################Amount by Which Weights and Biases are Altered##############################
+        self.INC_AMOUNT = 0.05
+    
+
+    def getOutput(self, input):
         ##########################################################################################
         ##############################Apply Learned Function (Using Guess Parameters)##############################
         ##########################################################################################
         ##############################Add Bias##############################
-        biasAdded = np.add(sumVector, biasGuess)
+        biasAdded = np.add(input, self.params.biases)
         ##############################Apply Activation Function##############################
         reluApplied = relu2DVector(biasAdded)
-        
         hiddenLayer.dataVector = reluApplied
         ##############################Dot Product Weights##############################
-        weightFactored = np.dot(reluApplied,  weightGuess)
+        output = np.dot(reluApplied,  self.params.weights)
+
         ##############################Output Guess##############################
-        bestGuess = np.copy(weightFactored)
-        ##############################Draw Current Neural Network##############################
-        hiddenLayer.displayData()
-        outputLayer.dataVector = np.around(bestGuess, 2)
-        outputLayer.displayData()
-        ##########################################################################################
-        ##############################Calculate Phi (Mean Squared Error)##############################
-        ##########################################################################################
-        ##############################Calculate Error##############################
-        errorVector = np.subtract(bestGuess, outputVector)
-        ##############################Calculate Squared Error##############################
-        errorVector = np.square(errorVector)
-        ##############################Calculate Mean Squared Error##############################
-        MSE = np.sum(errorVector) / 4
-        return MSE
-    ##############################Least MSE Per Iteration##############################
-    minLoss = 1 
-    ##########################################################################################
-    ##############################Back Propagation Loop##############################
-    ##########################################################################################
-    while minLoss > 0.01:
-        global bestText
-        ##############################Current Output With Least MSE (Most Desirable Phi)##############################
-        ##############################Update Display##############################
-        #bestText = "Best Guess: " + str(bestGuess)
-        #canvas.itemconfig(bestGuessLabel, text = bestText)
-        ##############################Update Function Display##############################
-        bestText = "Relu(" + str(sumVector) + " + " + \
-            str(biasGuess) + ") * " + str(weightGuess) + " = " + str(bestGuess)
-        canvas.itemconfig(bestGuessLabel, text = bestText)
-        guessPlot.addLine(sumVector, bestGuess)
-        window.update()
-        ##############################Reset Best MSE Each Iteration##############################
-        minLoss = 1
-        ##########################################################################################
-        ##############################Apply Loss Function (Least Mean Squared)##############################
-        ##########################################################################################
-        ##############################Calculate Loss with Increment Weight 1##############################
-        weightGuessCopy = np.copy(weightGuess)
-        weightGuess[0] = weightGuessCopy[0] + INC_AMOUNT
-        incW1 = testParams(sumVector, weightGuess, biasGuess)
-        ##############################Calculate Loss with Decrement Weight 1##############################
-        weightGuess[0] = weightGuessCopy[0] - INC_AMOUNT
-        decW1  = testParams(sumVector, weightGuess, biasGuess)
-        ##############################Calculate Loss with Increment Weight 2##############################
-        weightGuess = np.copy(weightGuessCopy)
-        weightGuess[1] = weightGuessCopy[1] + INC_AMOUNT
-        incW2 = testParams(sumVector, weightGuess, biasGuess)
-        ##############################Calculate Loss with Decrement Weight 2##############################
-        weightGuess[1] = weightGuessCopy[1] - INC_AMOUNT
-        decW2  = testParams(sumVector, weightGuess, biasGuess)
-        ##############################Calculate Loss with Increment Bias 1##############################
-        weightGuess = np.copy(weightGuessCopy)
-        biasGuessCopy = np.copy(biasGuess)
-        biasGuess[0] = biasGuessCopy[0] + INC_AMOUNT
-        incB1 = testParams(sumVector, weightGuess, biasGuess)
-        ##############################Calculate Loss with Decrement Bias 1##############################
-        biasGuess[0] = biasGuessCopy[0] - INC_AMOUNT
-        decB1  = testParams(sumVector,weightGuess, biasGuess)
-        ##############################Calculate Loss with Increment Bias 2##############################
-        biasGuess = np.copy(biasGuessCopy)
-        biasGuess[1] = biasGuessCopy[1] + INC_AMOUNT
-        incB2 = testParams(sumVector, weightGuess, biasGuess)
-        ##############################Calculate Loss with Decrement Bias 2##############################
-        biasGuess[1] = biasGuessCopy[1] - INC_AMOUNT
-        decB2  = testParams(sumVector,weightGuess, biasGuess)
-        biasGuess = np.copy(biasGuessCopy)
-        ##############################Determine Phi (Least MSE)##############################
-        minLoss = min(incW1, decW1, incW2, decW2, incB1, decB1, incB2, decB2)
-        ##########################################################################################
+        return output
+
+
+
+
+
+
+
+    def optParams(self):
+        paramVector = self.params.paramVector()
+
+        testVectors = []
+        for i in range(len(paramVector)):
+            testVector = np.copy(paramVector)
+            testVector[i] = paramVector[i] + self.INC_AMOUNT
+            testVectors.append(testVector)
+            testVector = np.copy(paramVector)
+            testVector[i] = paramVector[i] - self.INC_AMOUNT
+            testVectors.append(testVector)
+        lossVector = []
+        ##############################Test Each Parameter Set##############################
+        for testVector in testVectors:
+            self.params.updateParams(testVector)
+            output = self.getOutput(self.input)
+            #displayState(self, output)
+            loss = lossFunction(output, expected)
+            lossVector.append(loss)
         ##############################Back Propagation (Adjust Parameter Resulting Optimal Loss)##############################
-        ##########################################################################################
-        if minLoss == incW1:
-            weightGuess[0] = weightGuess[0] + INC_AMOUNT
-        elif minLoss == decW1:
-            weightGuess[0] = weightGuess[0] - INC_AMOUNT
-        elif minLoss == incW2:
-            weightGuess[1] = weightGuess[1] + INC_AMOUNT
-        elif minLoss == decW2:
-            weightGuess[1] = weightGuess[1] - INC_AMOUNT
-        elif minLoss == incB1:
-            biasGuess[0] = biasGuess[0] + INC_AMOUNT
-        elif minLoss == decB1:
-            biasGuess[0] = biasGuess[0] - INC_AMOUNT
-        elif minLoss == incB2:
-            biasGuess[1] = biasGuess[1] + INC_AMOUNT
-        elif minLoss == decB2:
-            biasGuess[1] = biasGuess[1] - INC_AMOUNT
-learn()
+        ##############################Determine Phi (Least MSE)##############################
+        self.params.updateParams(testVectors[np.argmin(lossVector)])
+        # self.bestGuess = self.testParams(self.sumVector, s)
+        print("New Params: ")
+        self.params.printParams()
+        print("New Phi: " + str(np.min(lossVector)))
+        return np.min(lossVector)
+            
+        
+        
+inputData = np.array([[0,0],[0,1],[1,0],[1,1]])
+expected = np.array([0,1,1,0])
+neural = Neural(inputData)
+while neural.optParams() > 0.1:
+    pass
+#learn()
 ##############################Animation Loop##############################
 while True:
     window.update()
